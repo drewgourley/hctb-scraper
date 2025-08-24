5a559c3cb5d36d8857607224740ef25f5146214d
Drew Gourley <drewgourley@gmail.com>
2025-08-24 19:42:25 -0400

    Change session building to fetch to reduce use of puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+import * as cheerio from 'cheerio';
+import cron, { type TaskContext } from 'node-cron';
 import dotenv from 'dotenv';
 import express, { type Request, type Response, type Express } from 'express';
-import fetch from 'node-fetch';
+import fetch, { type Response as FetchResponse } from 'node-fetch';
 import http from 'http';
-import puppeteer, { Browser, type Cookie, type LaunchOptions, Page } from 'puppeteer';
-import cron, { type TaskContext } from 'node-cron';
+import puppeteer, { type Browser, type Cookie, type LaunchOptions, type Page } from 'puppeteer';
 import type { Child, Defaults, Location, Session, Time } from './models.js';
 
 dotenv.config({ quiet: true });
@@ -53,33 +54,44 @@ async function login(): Promise<void> {
     await page.type('#ctl00_ctl00_cphWrapper_cphContent_tbxPassword', defaults.HCTB_PASSWORD);
     await page.type('#ctl00_ctl00_cphWrapper_cphContent_tbxAccountNumber', defaults.HCTB_SCHOOLCODE);
     await page.click('#ctl00_ctl00_cphWrapper_cphContent_btnAuthenticate');
-    await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+    await page.waitForNavigation({ waitUntil: 'load' });
     const cookies: Cookie[] = await browser.cookies();
+    await browser.close();
     if (cookies?.find((cookie) => cookie.name === '.ASPXFORMSAUTH')) {
       if (DEV) console.debug('--Got session cookie');
       let cookiestring: string = '';
-      for (const cookie of cookies) { cookiestring += `${cookie.name}=${cookie.value}; `; };
-      const children: Child[] = await page.$$eval(
-        '#ctl00_ctl00_cphWrapper_cphControlPanel_ddlSelectPassenger option',
-        (options: HTMLOptionElement[]): Child[] => {
-          return options.map((option: HTMLOptionElement): Child => {
-            return { name: option.innerHTML, id: option.value, active: true };
-          });
-        }
-      );
-      const time: Time = await page.$eval(
-        '#ctl00_ctl00_cphWrapper_cphControlPanel_ddlSelectTimeOfDay option[selected="selected"]',
-        (option: HTMLOptionElement): Time => {
-          return { label: option.innerHTML, id: option.value };
-        }
-      );
-      if (children && time) {
-        if (DEV) {
-          for (const child of children) {
-            console.debug(`--Found child - Name: ${child.name}, ID: ${child.id}`);
+      let children: Child[] = [];
+      let time: Time | undefined;
+      for (const cookie of cookies) { cookiestring += `${cookie.name}=${cookie.value}; `; }
+      await fetch('https://login.herecomesthebus.com/Map.aspx', {
+        headers: { cookie: cookiestring },
+        method: 'GET',
+      })
+      .then((res: FetchResponse) => {
+        if (res?.ok) {
+          return res.text();
+        } else {
+          throw new Error(res?.status?.toString());
+        };
+      })
+      .then((text: string) => {
+        if (text) {
+          if (DEV) console.debug('--Fetched Map page');
+          const $ = cheerio.load(text);
+          for (const option of $('#ctl00_ctl00_cphWrapper_cphControlPanel_ddlSelectPassenger option')) {
+            const child: Child = { name: $(option).text(), id: $(option).val() as string, active: true };
+            if (DEV) console.debug(`--Found child - Name: ${child.name}, ID: ${child.id}`);
+            children?.push(child);
           }
-          console.debug(`--Found time - Label: ${time.label}, ID: ${time.id}`);
-        }
+          const timeoption = $('#ctl00_ctl00_cphWrapper_cphControlPanel_ddlSelectTimeOfDay option[selected="selected"]');
+          time = { label: timeoption.text(), id: timeoption.val() as string };
+          if (DEV) console.debug(`--Found time - Label: ${time.label}, ID: ${time.id}`);
+          return text;
+        } else {
+          throw new Error('Failed to fetch Map page');
+        };
+      });
+      if (children.length && time) {
         healthy = true;
         session = { cookiestring, children, time };
         console.info('  Session established');
@@ -103,20 +115,8 @@ async function scrape(child: Child): Promise<Location | undefined> {
   try {
     await fetch('https://login.herecomesthebus.com/Map.aspx/RefreshMap', {
       headers: {
-        accept: 'application/json, text/javascript, */*; q=0.01',
-        'accept-language': 'en-US,en;q=0.9,ja;q=0.8',
         'content-type': 'application/json; charset=UTF-8',
-        'sec-ch-ua':
-          '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'x-requested-with': 'XMLHttpRequest',
         cookie: session?.cookiestring ?? '',
-        Referer: 'https://login.herecomesthebus.com/Map.aspx',
-        'Referrer-Policy': 'strict-origin-when-cross-origin',
       },
       body: JSON.stringify({
         legacyID: child.id,
@@ -126,7 +126,7 @@ async function scrape(child: Child): Promise<Location | undefined> {
       }),
       method: 'POST',
     })
-      .then((res) => {
+      .then((res: FetchResponse) => {
         if (res?.ok) {
           return res.json();
         } else if (res?.status === 401) {
