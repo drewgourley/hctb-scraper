46a5090dfea8cbc5858aa1e49bbea3a6ee84eb97
Drew Gourley <drewgourley@gmail.com>
2025-08-28 23:54:57 -0400

    remove unused import

--- hctb-scraper/src/index.ts
+++ hctb-scraper/src/index.ts
@@ -1,7 +1,7 @@
 import { parse, type HTMLElement } from 'node-html-parser';
 import cron, { type TaskContext } from 'node-cron';
 import fetch, { type Response as FetchResponse } from 'node-fetch';
-import { TrueFalseString, type Child, type Config, type Location, type RefreshMapInput, type Session, type Sessions } from './models.js';
+import { TrueFalseString, type Child, type Config, type Location, type RefreshMapInput, type Sessions } from './models.js';
 
 const config: Config = process.env as unknown as Config;
 const defaultlocation: Location = { default: true, lat: config.DEFAULT_LAT, lon: config.DEFAULT_LON };
